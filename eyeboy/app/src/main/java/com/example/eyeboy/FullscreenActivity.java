8b45b82cb5af52a5db148d03f03e02d7d0daaa3a
ssshahzod <55853125+ssshahzod@users.noreply.github.com>
2020-10-05 16:23:34 +0300

    .

--- eyeboy/app/src/main/java/com/example/eyeboy/FullscreenActivity.java
+++ eyeboy/app/src/main/java/com/example/eyeboy/FullscreenActivity.java
@@ -44,7 +44,7 @@ public class FullscreenActivity extends AppCompatActivity {
                 }
             }
 
-        }
+        });
     }
 
 
--- eyeboy/app/src/main/java/com/example/eyeboy/tacker.java
+++ eyeboy/app/src/main/java/com/example/eyeboy/tacker.java
@@ -2,14 +2,21 @@ package com.example.eyeboy;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.widget.AppCompatEditText;
+
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
@@ -25,13 +32,12 @@ import java.util.ArrayList;
 
 
 public class tacker extends AppCompatActivity {
-    public String side;
-    public int blinks, blink;
+    public int side;
+    public int eyeclosed;
+    public int blinks, blink, coord;
 
     private static final String TAG = "tracker";
-    ImageView imageView;
-    EditText textViewL;
-    EditText textViewR;
+
 
     //For looking logs
     ArrayAdapter adapter;
@@ -46,7 +52,23 @@ public class tacker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.tacker);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Button exitbtn = (Button)findViewById(R.id.exitbtn);
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //exit btn, back to menu
+
+                try{
+                    Intent intent = new Intent(tacker.this, FullscreenActivity.class);
+                    startActivity(intent);finish();
+                } catch (Exception e){
+
+                }
+            }
+        });
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
@@ -54,9 +76,7 @@ public class tacker extends AppCompatActivity {
         }
         else {
 
-            //    imageView = findViewById(R.id.imageView);
-            textViewL = findViewById(R.id.LeftText);
-            textViewR = findViewById(R.id.RightText);
+
             adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
             createCameraSource();
         }
@@ -75,30 +95,46 @@ public class tacker extends AppCompatActivity {
         @Override
         public void onUpdate(Detector.Detections<Face> detections, Face face) {
             if (face.getIsLeftEyeOpenProbability() > THRESHOLD || face.getIsRightEyeOpenProbability() > THRESHOLD) {
-                Log.i(TAG, "onUpdate: Eyes Detected");
-
-
-                showStatus(String.valueOf(face.getEulerY()));
-
-
-
+                //Log.i(TAG, "onUpdate: Eyes Detected");
+                blinks = 0;
+                coord = (int) face.getEulerY();
+                if (coord > 14) {
+                    side = 1;
+                } else{
+                    side = 0;
+                }
             }
-            else {
-                //добавить одну булевую переменную которая будет менятся в if и else чтобы считались именно моргания
+            if ((face.getIsLeftEyeOpenProbability() > THRESHOLD || face.getIsRightEyeOpenProbability() > THRESHOLD) && eyeclosed == 1) {
+
+                //Log.i(TAG, "eyeclosed:"+ String.valueOf(eyeclosed));
+                //Log.i(TAG, "side:"+ String.valueOf(side));
+                //Log.i(TAG, "blinks:"+ String.valueOf(blinks));
+                if (side == 1 && blink == 1) {
+                    Log.i(TAG, " Left shot! ");
+                    blink = 0;
+                    eyeclosed = 0;
+
+                }
+                if (side == 0 && blink == 1){
+                    Log.i(TAG, " Right Shot! ");
+                    blink = 0;
+                    eyeclosed = 0;
+                }
+            }
+            if (!(face.getIsLeftEyeOpenProbability() > THRESHOLD || face.getIsRightEyeOpenProbability() > THRESHOLD) && eyeclosed == 0) {
+                //Log.i(TAG, "eyeclosed:"+ String.valueOf(eyeclosed));
                 blinks = blinks + 1;
-                if (blinks > 10){
+                if (blinks > 8) {
                     blink += 1;
                     blinks = 0;
+                    eyeclosed = 1;
+
                 }
-                showStatus(" Eyes closed "+String.valueOf(blink));
+
             }
         }
 
-        @Override
-        public void onMissing(Detector.Detections<Face> detections) {
-            super.onMissing(detections);
-            showStatus("  Face Not Detected yet!");
-        }
+
 
         @Override
         public void onDone() {
@@ -145,14 +181,8 @@ public class tacker extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    public void showStatus(final String message) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                textView.setText(message);
-            }
-        });
-    }
+
+
 }
 
 
